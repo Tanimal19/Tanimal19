89195052a1361e9b4b233145e0b673a72d3d0bc1
Tanimal <58576763+Tanimal19@users.noreply.github.com>
2024-03-21 21:13:10 +0800

    Update fetch_spotify_data.js

--- .github/workflows/fetch_spotify_data.js
+++ .github/workflows/fetch_spotify_data.js
@@ -7,58 +7,89 @@ var client_secret = 'dadbdbf6451445d8a04497182501a203'; // Your secret
 
 var refresh_token = 'AQAhr3J9Z7c4wdKMuwxVQhNXjIVIWfgGuZuIiGTjm6RoqocPbsVcGQxa-7NfOC8t9-FIsMEg6N7Eky5H8V8wWSnonTL0Ewedn4DrGF7iXqVk89A4k6YWmLxi2Rm-9PqYlhg';
 
-axios.post('https://accounts.spotify.com/api/token', querystring.stringify({
-    grant_type: 'refresh_token',
-    refresh_token: refresh_token
-}), {
-    headers: {
+async function fetchSpotifyStats(endpoint) {
+  try {
+    const tokenResponse = await axios.post('https://accounts.spotify.com/api/token', querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }), {
+      headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
-    }
-})
-.then(response => {
-    const access_token = response.data.access_token;
-
-    const endpoint = 'https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=5&offset=0';
-
-    axios.get(endpoint, { 
-        headers: {
-            'Authorization': 'Bearer ' + access_token
-        },
-    })
-    .then(response => {
-        // Handle successful response
-        var newData;
-        var html = "<table>\n";
-        const itemList = response.data.items;
-        itemList.forEach(function (i) {
-          html += "<tr><img src='" + i.images[0].url + "' width='100px'><div>" + i.name + "</div></tr>\n";
-        });
-        html += "</table>";
-
-        const data = fs.readFileSync('README.md', 'utf8');
-        const regex = /<div id="spotify">([\s\S]*?)<\/div>/;
-        const match = data.match(regex);
-        if (match) {
-             newData = data.replace(match[0], `<div id="spotify">${html}</div>`);
-        }
-
-        console.log(data);
-        console.log(newData);
-        
-        fs.writeFile('TEST.md', (newData ? newData:data), (err) => {
-            if (err) {
-                console.error('Failed to write Markdown file:', err);
-            } else {
-                console.log('Markdown file has been written successfully!');
-            }
-        });
-    })
-    .catch(error => {
-        // Handle error
-        console.error('Error fetching playlist items:', error);
+      }
     });
-})
-.catch(error => {
-    console.error('Error refreshing token:', error);
-});
+
+    const access_token = tokenResponse.data.access_token;
+
+    const response = await axios.get(endpoint, {
+      headers: {
+        'Authorization': 'Bearer ' + access_token
+      },
+    });
+
+    return response.data.items;
+  } catch (error) {
+    console.error('Error fetching Spotify stats:', error);
+    return [];
+  }
+}
+
+async function main() {
+  // Fetch Spotify Data
+  const url_art = 'https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=5&offset=0';
+  const topArtistList = await fetchSpotifyStats(url_art);
+  var topArtistHtml;
+
+  if (topArtistList.length > 0) {
+    topArtistHtml = "<div>Top Artists</div><ol>\n";
+
+    topArtistList.forEach(function (i) {
+      topArtistHtml += "<li>" + i.name + "</li>\n";
+    });
+
+    topArtistHtml += "</ol>";
+  }
+
+  const url_track = 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10&offset=0';
+  const topTrackList = await fetchSpotifyStats(url_track);
+  var topTrackHtml;
+
+  if (topTrackList.length > 0) {
+    topTrackHtml = "<div>Top Tracks</div><ol>\n";
+
+    topTrackList.forEach(function (i) {
+      topTrackHtml += "<li><div><strong>" + i.name + "</strong></div><br><div>";
+
+      const artists = i.artists;
+      artists.forEach(function (a) {
+        topTrackHtml += a.name;
+      });
+
+      topTrackHtml += "</div>\n";
+
+    });
+
+    topTrackHtml += "</ol>";
+  }
+
+  // Modify Readme Content
+  const finalHtml = topArtistHtml + topTrackHtml;
+  var newData;
+  const data = fs.readFileSync('README.md', 'utf8');
+  const regex = /<div id="spotify">([\s\S]*?)<\/div>/;
+  const match = data.match(regex);
+    
+  if (match) {
+    newData = data.replace(match[0], `<div id="spotify">${finalHtml}</div>`);
+  }
+    
+  fs.writeFile('TEST.md', (newData ? newData : data), (err) => {
+    if (err) {
+      console.error('Failed to write Markdown file:', err);
+    } else {
+      console.log('Markdown file has been written successfully!');
+    }
+  });
+}
+
+main();
